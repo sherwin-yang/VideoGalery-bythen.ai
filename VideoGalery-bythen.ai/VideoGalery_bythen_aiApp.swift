b47ffc09fcb6052334f3bf8691bd47287394a6fb
Sherwin Yang <sher.yang79@gmail.com>
2024-09-12 18:18:51 +0700

    Initial Commit

---
+++ VideoGalery-bythen.ai/VideoGalery_bythen_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoGalery_bythen_aiApp.swift
+//  VideoGalery-bythen.ai
+//
+//  Created by Sherwin Yang on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoGalery_bythen_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
